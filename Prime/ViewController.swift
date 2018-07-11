2140a04d23b6c9b5433138c76e199c4780408deb
Marcelo Sampaio <marcelo@artlogica.com.br>
2018-07-11 15:21:47 -0300

    added some prime functions🎃

--- Prime/ViewController.swift
+++ Prime/ViewController.swift
@@ -10,16 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var number: UITextField!
+    
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - UI Actions
+    @IBAction func proceed(_ sender: Any) {
+        if (number.text?.isEmpty)! {
+            return
+        }
+        print("proceed action")
+        let result = isPrime(Int(number.text!)!)
+        
     }
+    
+    // MARK: - Prime Engine Helper
+    private func isPrime(_ number: Int) -> Bool {
+        
+        for i in (1...number).reversed() {
+            print("👻 i: \(i)")
+        }
+        
+        return false
+    }
+    
+    
+    private func nextPrime(_ number: Int) -> Int {
+        
+        var found = false
+        var i = 0
+        while found == false {
+            print("☠️..... looping \(i)")
+            i = i + 1
+            
+            if i>2 {
+                found = true
+            }
 
+        }
+    
+        return 0
+    }
+    
+    
+    
+    
 
 }
 
